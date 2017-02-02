a9dc56381b66998ee98dc493d77fd5ef1418a4f9
steveq <steveq@nist.gov>
2017-02-02 10:56:42 -0500

    Added log save.

--- src/gov/nist/appvet/tool/androwarn/Properties.java
+++ src/gov/nist/appvet/tool/androwarn/Properties.java
@@ -35,7 +35,7 @@ import java.util.ArrayList;
 public class Properties {
 
 	/** Github release version number. */
-	public static final String version = "1.0.3";
+	public static final String version = "1.1";
 
 	/*
 	 * CHANGE: Declare tool-specific environment variables. Refactor
